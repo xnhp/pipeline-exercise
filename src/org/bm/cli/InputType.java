1ef0f299c8a209fece5d16e574b77652e99cf43a
Benjamin Moser <benjamin.2.moser@uni.kn>
2020-03-12 17:26:43 +0100

    modelling of data input types

---
+++ src/org/bm/cli/InputType.java
@@ -0,0 +1,14 @@
+package org.bm.cli;
+
+/**
+ * Note that this values are case-insensitive mapping targets for CLI options, using
+ * an option of `CommandLine`.
+ *
+ * If argument strings that can not be expressed as enum constants are required, we'd need
+ * a separate data structure or procedure for mapping.
+ *
+ * @author Benjamin Moser.
+ */
+public enum InputType {
+    STRING, INT, DOUBLE
+}
